ab17d3683e110edffe918cf9d24d84317c45decd
Vadim Savinov <vasaykh@yandex.ru>
2022-08-15 01:46:53 +0500

    добавить загрузку 6 карточек

--- scripts/index.js
+++ scripts/index.js
@@ -10,24 +10,91 @@ const formEditProfile = popupEditProfile.querySelector(
 const inputName = popupEditProfile.querySelector('.form__item_user_name');
 const inputJob = popupEditProfile.querySelector('.form__item_user_job');
 
+const initialCards = [
+  {
+    name: 'з. Таганай',
+    link: './images/taganay-national-park.jpg',
+  },
+  {
+    name: 'н.п. Югыд Ва',
+    link: './images/yugyd-va-national-park.jpg',
+  },
+  {
+    name: 'Мань-Пупу-нёр',
+    link: './images/manpupuner-plateau.jpg',
+  },
+  {
+    name: 'гора Шихан',
+    link: './images/lake-arakul-and-arakul-shikhan.jpg',
+  },
+  {
+    name: 'р. Чусовая',
+    link: './images/chusovaya-river.jpg',
+  },
+  {
+    name: 'оз. Тургояк',
+    link: './images/lake-turgoyak.jpg',
+  },
+];
+
 const cardContainer = document.querySelector('.cards');
 
-//открытие попап
+//открытие popup
 function openPopup(popup) {
   popup.classList.add('popup_opened');
 }
 
-//закрытие попап
+//закрытие popup
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
 }
 
-//обработчик кнопки редактирования профиля (открытия попапа редактирования профиля)
+//обновление полей формы редактю профиля при открытии popup
+function updateFormEditProfile() {
+  inputName.value = profileName.textContent;
+  inputJob.value = profileJob.textContent;
+}
+
+//функция submit для формы редактирования профиля
+function formEditProfileSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileJob.textContent = inputJob.value;
+  closePopup(popupEditProfile);
+}
+
+//функция добавления карточки с местом, аргумент - объект с двумя ключами name и link
+function addCard(item) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate
+    .querySelector('.cards__item')
+    .cloneNode(true);
+  cardElement.querySelector('.cards__name').textContent = item.name;
+  cardElement.querySelector('.cards__image').src = item.link;
+  cardElement
+    .querySelector('.cards__like-button')
+    .addEventListener('click', function (evt) {
+      evt.target.classList.toggle('cards__like-button_liked');
+    });
+
+  cardContainer.prepend(cardElement);
+}
+
+//обработчик кнопки редактирования профиля (открытия popup редактирования профиля)
 editButton.addEventListener('click', function () {
+  updateFormEditProfile();
   openPopup(popupEditProfile);
 });
 
-//обработчик кнопки закрытия попап редактирования профиля
+//обработчик кнопки закрытия popup редактирования профиля
 popupCloseButton.addEventListener('click', function () {
   closePopup(popupEditProfile);
+});
+
+//обработчик submit для формы редактирования профиля
+formEditProfile.addEventListener('submit', formEditProfileSubmitHandler);
+
+//добавление 6 карточек при загрузке страницы
+initialCards.forEach((item) => {
+  addCard(item);
 });
