e23aaeb567429175ac37bf9baf0f8bf5c417987c
Vadim Savinov <vasaykh@yandex.ru>
2022-08-16 00:53:10 +0500

    добавить popup новых карточек и удаление карточек

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,9 @@ const editButton = profile.querySelector('.profile__edit-button');
 const profileName = profile.querySelector('.profile__name');
 const profileJob = profile.querySelector('.profile__job');
 const popupEditProfile = document.querySelector('.popup_content_edit-profile');
-const popupCloseButton = popupEditProfile.querySelector('.popup__close-button');
+const popupCloseButtonEdit = popupEditProfile.querySelector(
+  '.popup__close-button'
+);
 const formEditProfile = popupEditProfile.querySelector(
   '.form_content_edit-profile'
 );
@@ -38,6 +40,12 @@ const initialCards = [
 ];
 
 const cardContainer = document.querySelector('.cards');
+const addButton = profile.querySelector('.profile__add-button');
+const popupAddCard = document.querySelector('.popup_content_add-card');
+const formAddCard = popupAddCard.querySelector('.form_content_add-card');
+const inputCardName = popupAddCard.querySelector('.form__item_place_name');
+const inputCardLink = popupAddCard.querySelector('.form__item_place_link');
+const popupCloseButtonAdd = popupAddCard.querySelector('.popup__close-button');
 
 //открытие popup
 function openPopup(popup) {
@@ -63,6 +71,17 @@ function formEditProfileSubmitHandler(evt) {
   closePopup(popupEditProfile);
 }
 
+//функция удаления карточки из имеющегося набора на момент объявления функции
+function delCard() {
+  let delCardButtons = cardContainer.querySelectorAll('.cards__delete-button');
+  //обработчик кнопки удаления карточки
+  delCardButtons.forEach((item) => {
+    item.addEventListener('click', (evt) => {
+      evt.target.parentNode.remove();
+    });
+  });
+}
+
 //функция добавления карточки с местом, аргумент - объект с двумя ключами name и link
 function addCard(item) {
   const cardTemplate = document.querySelector('#card-template').content;
@@ -78,6 +97,18 @@ function addCard(item) {
     });
 
   cardContainer.prepend(cardElement);
+
+  delCard();
+}
+
+//функция submit для формы добавления карточки
+function formAddCardSubmitHandler(evt) {
+  evt.preventDefault();
+  const item = {};
+  item.name = inputCardName.value;
+  item.link = inputCardLink.value;
+  addCard(item);
+  closePopup(popupAddCard);
 }
 
 //обработчик кнопки редактирования профиля (открытия popup редактирования профиля)
@@ -87,12 +118,25 @@ editButton.addEventListener('click', function () {
 });
 
 //обработчик кнопки закрытия popup редактирования профиля
-popupCloseButton.addEventListener('click', function () {
+popupCloseButtonEdit.addEventListener('click', function () {
   closePopup(popupEditProfile);
 });
 
 //обработчик submit для формы редактирования профиля
 formEditProfile.addEventListener('submit', formEditProfileSubmitHandler);
+
+//обработчик кнопки открытия popup для добавления карточки
+addButton.addEventListener('click', function () {
+  openPopup(popupAddCard);
+});
+
+//обработчик кнопки закрытия popup добавления карточки
+popupCloseButtonAdd.addEventListener('click', function () {
+  closePopup(popupAddCard);
+});
+
+//обработчик submit для формы добавления карточки
+formAddCard.addEventListener('submit', formAddCardSubmitHandler);
 
 //добавление 6 карточек при загрузке страницы
 initialCards.forEach((item) => {
