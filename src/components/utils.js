87234d9f35a11b82c4d4a74a6093a178edb2c6ee
Vadim Savinov <vasaykh@yandex.ru>
2022-10-28 18:37:39 +0500

    fix: удалить паттерн модуль в utils.js

--- src/components/utils.js
+++ src/components/utils.js
@@ -4,35 +4,30 @@ import { openPopup, popupUpdateAvatar } from '../components/modal.js';
 
 export { initialAvatar, initialUser };
 
-const ChartProfile = (function () {
-  const avatarImage = document.querySelector('.profile__avatar');
-  return {
-    //функция добавления атрибутов в img аватара
-    initialAvatar: function (item) {
-      avatarImage.src = item.avatar;
-      avatarImage.alt = item.name;
-      //слушатель наведения мыши на аватар с изменением класса псевдоэлемента
-      avatarImage.addEventListener('mouseover', () => {
-        document.querySelector('.profile__wrapper-avatar').className =
-          'profile__wrapper-avatar new';
-      });
-      //слушатель выхода мыши из аватара с возвращением прежднего класса псевдоэлемента
-      avatarImage.addEventListener('mouseout', () => {
-        document.querySelector('.profile__wrapper-avatar.new').className =
-          'profile__wrapper-avatar';
-      });
-      //слушатель клика по аватару с открытием попапа
-      avatarImage.addEventListener('click', () => {
-        openPopup(popupUpdateAvatar);
-      });
-    },
-    //функция добавления атрибутов в name и about пользователя
-    initialUser: function (item) {
-      profileName.textContent = item.name;
-      profileJob.textContent = item.about;
-    },
-  };
-})();
+const avatarImage = document.querySelector('.profile__avatar');
 
-const initialAvatar = ChartProfile.initialAvatar;
-const initialUser = ChartProfile.initialUser;
+  //функция добавления атрибутов в img аватара
+ const initialAvatar = function (item) {
+    avatarImage.src = item.avatar;
+    avatarImage.alt = item.name;
+    //слушатель наведения мыши на аватар с изменением класса псевдоэлемента
+    avatarImage.addEventListener('mouseover', () => {
+      document.querySelector('.profile__wrapper-avatar').className =
+        'profile__wrapper-avatar new';
+    });
+    //слушатель выхода мыши из аватара с возвращением прежднего класса псевдоэлемента
+    avatarImage.addEventListener('mouseout', () => {
+      document.querySelector('.profile__wrapper-avatar.new').className =
+        'profile__wrapper-avatar';
+    });
+    //слушатель клика по аватару с открытием попапа
+    avatarImage.addEventListener('click', () => {
+      openPopup(popupUpdateAvatar);
+    });
+  }
+  //функция добавления атрибутов в name и about пользователя
+  const initialUser = function (item) {
+    profileName.textContent = item.name;
+    profileJob.textContent = item.about;
+  }
+
