96a58ca6a80bf996fbf864149bf9cd2c5e0d3c0f
Vadim Savinov <vasaykh@yandex.ru>
2022-12-03 02:34:51 +0500

    fix: npm install gh-pages

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,21 +1,22 @@
-import { Popup } from "./Popup";
+import { Popup } from './Popup';
 
-export class PopupWithImage extends Popup{
+export class PopupWithImage extends Popup {
+  static _imageClass = 'popup__img';
+  static _imageCaptionClass = 'popup__subtitle';
 
-    static _imageClass =  'popup__img';
-    static _imageCaptionClass = 'popup__subtitle';
+  constructor(selector) {
+    super(selector);
+    this._modal = document.querySelector(selector);
+    this._image = document.querySelector(`.${PopupWithImage._imageClass}`);
+    this._imageCaption = document.querySelector(
+      `.${PopupWithImage._imageCaptionClass}`
+    );
+  }
 
-    constructor (selector) {
-        super(selector);
-        this._modal = document.querySelector(selector);
-        this._image = document.querySelector(`.${PopupWithImage._imageClass}`);
-        this._imageCaption = document.querySelector(`.${PopupWithImage._imageCaptionClass}`);
-    }
-
-    open(cardLink, cardName) {
-        this._image.src = cardLink;
-        this._image.alt = cardName;
-        this._imageCaption.textContent = cardName;
-        super.open();
-    }
+  open(cardLink, cardName) {
+    this._image.src = cardLink;
+    this._image.alt = cardName;
+    this._imageCaption.textContent = cardName;
+    super.open();
+  }
 }
